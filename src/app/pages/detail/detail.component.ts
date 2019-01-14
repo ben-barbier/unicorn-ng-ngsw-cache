f73f9e14cf363c04af8d99ef2f37c77680fb886c
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2019-01-15 00:19:06 +0100

    :sparkles: add picture for missing unicorns

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,6 +1,9 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {ActivatedRoute, Params, Router} from '@angular/router';
 import {UnicornsService} from '../../shared/services/unicorns.service';
+import {catchError} from 'rxjs/operators';
+import {Observable, of, Subject, throwError} from 'rxjs';
+import {Unicorn} from '../../shared/models/unicorn.model';
 
 @Component({
     selector: 'app-detail',
@@ -9,15 +12,22 @@ import {UnicornsService} from '../../shared/services/unicorns.service';
 })
 export class DetailComponent {
 
-    public unicorn;
-    public id;
+    public unicorn: Observable<Unicorn>;
+    public id: number;
+    public missing = false;
 
     constructor(private unicornsService: UnicornsService,
                 private route: ActivatedRoute,
                 private router: Router) {
         this.route.params.subscribe((params: Params) => {
+            this.missing = false;
             this.id = params.id;
-            this.unicorn = this.unicornsService.get(params.id);
+            this.unicorn = this.unicornsService.get(params.id).pipe(
+                catchError(e => {
+                    this.missing = true;
+                    return throwError(e);
+                })
+            );
         });
     }
 
